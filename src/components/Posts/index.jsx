4eb155a05d56c9bf06b64cc2e3b2bbdb1e7e02e1
Matheus-daSilva <matheus2001silva@live.com>
2022-06-14 19:14:57 -0300

    fix: postbody

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -2,13 +2,16 @@ import * as S from "./styles";
 import urlMetadata from "url-metadata";
 
 export default function Posts() {
+    const { name, picture, link, description} = props;
     return (
         <S.Container>
-            <S.UserPicture />
+            <S.UserPicture src={picture}/>
             <S.PostBody>
-                <h2>Juvenal Juvêncio</h2>
-                <p>Muito maneiro esse tutorial de material UI com React, deem uma olhada! #React #material</p>
-                <S.linkCard></S.linkCard>
+                <h2>{name}</h2>
+                <p>{description}</p>
+                <S.linkCard>
+                    
+                </S.linkCard>
             </S.PostBody>
         </S.Container>
     )
--- src/pages/Timeline/index.jsx
+++ src/pages/Timeline/index.jsx
@@ -2,15 +2,34 @@ import * as S from "./styles";
 import Header from "../../components/Header";
 import NewPost from "../../components/NewPost";
 import Posts from "../../components/Posts";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Timeline() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const request = axios.get("http://localhost:5000/timeline");
+
+        request.then(response => {
+            const { data } = response;
+            setPosts(...data);
+        })
+        request.catch(() => {
+            alert("An error occured while trying to fetch the posts, please refresh the page")
+        })
+    })
     return (
         <S.Container>
             <S.PostsColumn>
                 <Header />
                 <S.H2>timeline</S.H2>
                 <NewPost />
-                <Posts />
+                {items.map(item => {
+                    return (
+                        <Posts name={item.name} picture={item.picture} link={item.link} description={item.description}/>
+                    )
+                })}
             </S.PostsColumn>
         </S.Container>
     )
