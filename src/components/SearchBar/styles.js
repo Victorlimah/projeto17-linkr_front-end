e68a5902dd4ed73b88e6d007baf8375a3c80414f
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-06-20 00:20:36 -0300

    Fix adjust search bar

--- src/components/SearchBar/styles.js
+++ src/components/SearchBar/styles.js
@@ -3,9 +3,10 @@ import styled from "styled-components";
 export const SearchAll = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100vw;
+  width: 600px;
   justify-content: center;
   border-radius: 8px;
+  position: relative;
 
   a:link {
     text-decoration: none;
@@ -22,14 +23,14 @@ export const SearchAll = styled.div`
 
 export const SearchEmpty = styled.div`
   display: flex;
-  width: 80%;
+  width: 600px;
   z-index: 5;
   justify-content: center;
   border-radius: 8px;
   position: fixed;
 
   .input {
-    width: 529px;
+    width: 500px;
     height: 30px;
     background: #ffffff;
     border-radius: 8px;
@@ -63,16 +64,17 @@ export const SearchEmpty = styled.div`
 
   @media (max-width: 829px) {
     position: fixed;
-    top: 75px;
-    width: 95%;
-    left: 0px;
-  
+    top: 55px;
+    left: 0;
+    right: 0;
+    width: 100%;
 
     .input {
-      width: 95%;
+      width: 500px;
       height: 40px;
       left: 10px;
       right: 10px;
+      margin: 0 auto;
     }
   }
 
@@ -88,9 +90,9 @@ export const SearchEmpty = styled.div`
 export const Search = styled.div`
   display: flex;
   flex-direction: column;
-  position: absolute;
+  position: relative;
   top: -60px;
-  width: 563px;
+  width: 600px;
   justify-content: center;
   margin-top: 10px;
   padding-top: 30px;
@@ -100,11 +102,10 @@ export const Search = styled.div`
   border-radius: ${(props) => (props.isEmpty ? "0px 0px 8px 8px" : "8px")};
 
   @media (max-width: 829px) {
-    top: 75px;
-    width: 100%;
+    top: 50px;
+    width: 500px;
     position: absolute;
     top: -5px;
-    left: 10px;
-    right: 10px;
+    z-index: 4;
   }
 `;
