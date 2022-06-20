14613fd934622146775b26330345ceac935d8547
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-06-19 21:01:09 -0300

    Fix styles searchBar

--- src/components/SearchBar/styles.js
+++ src/components/SearchBar/styles.js
@@ -7,18 +7,18 @@ export const SearchAll = styled.div`
   justify-content: center;
   border-radius: 8px;
 
-  a:link{
-  text-decoration: none;
+  a:link {
+    text-decoration: none;
   }
 
-  a:visited{
-  text-decoration: none;
+  a:visited {
+    text-decoration: none;
   }
 
-  a:hover{
-  text-decoration: none;
+  a:hover {
+    text-decoration: none;
   }
-`
+`;
 
 export const SearchEmpty = styled.div`
   display: flex;
@@ -28,46 +28,47 @@ export const SearchEmpty = styled.div`
   border-radius: 8px;
   position: fixed;
 
-  .input{
-  width: 529px;
-  height: 45px;
-  margin: 0 auto 0;
-  padding-right: 17px;
-  padding-left: 17px;
-  background: #FFFFFF;
-  border-radius: 8px;
-  border: 0.1px solid #FFFFFF;
-  position: relative;
+  .input {
+    width: 529px;
+    height: 30px;
+    margin: 0 auto 0;
+    padding-right: 17px;
+    padding-left: 17px;
+    background: #ffffff;
+    border-radius: 8px;
+    border: 0.1px solid #ffffff;
+    position: relative;
   }
 
-  input::placeholder{
-  font-family: 'Lato';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 19px;
-  line-height: 23px;
-  color: #C6C6C6;
+  input::placeholder {
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19px;
+    line-height: 23px;
+    color: #c6c6c6;
   }
 
-  .icon{
-    font-size: 30px;
+  .icon {
+    font-size: 28px;
     right: 10px;
-    top: 8px;
+    top: 4px;
     position: absolute;
-    color: #C6C6C6;
+    color: #c6c6c6;
   }
-`
+`;
 
 export const Search = styled.div`
   display: flex;
   flex-direction: column;
+  position: absolute;
+  top: -60px;
   width: 563px;
   justify-content: center;
   margin-top: 10px;
-  padding-top: 55px;
-  background: ${props => props.isEmpty ? "" : "#E7E7E7"};
-  margin-top: ${props => props.isEmpty ? "" : "118px"};
-  padding-bottom: ${props => props.isEmpty ? "" : "23px"};
-  border-radius: ${props => props.isEmpty ? "0px 0px 8px 8px" : "8px"};
+  padding-top: 30px;
+  background: ${(props) => (props.isEmpty ? "" : "#E7E7E7")};
+  margin-top: ${(props) => (props.isEmpty ? "" : "88px")};
+  padding-bottom: ${(props) => (props.isEmpty ? "" : "23px")};
+  border-radius: ${(props) => (props.isEmpty ? "0px 0px 8px 8px" : "8px")};
 `;
-
