2958dda259c8b57af77d66555be34763300d690e
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-06-23 04:23:55 -0300

    add: follow users

--- src/pages/TimelineUser/index.jsx
+++ src/pages/TimelineUser/index.jsx
@@ -15,27 +15,32 @@ export default function TimelineUser() {
     const [load, setLoad] = useState(false)
     const [publish, setPublish] = useState(false);
     const [following, setFollowing] = useState(false);
-    const { data, setData} = useContext(DataContext);
+    const { data, setData } = useContext(DataContext);
     const navigate = useNavigate()
     const API = data.API;
 
     const { id } = useParams();
-    
-    useEffect(() => {
-      const token = localStorage.getItem("token").split(".")[1];
-      if (!token) navigate("/signin");
-      let user = window.atob(token);
-      const { picture, id, username } = JSON.parse(user);
 
-      setData({ ...data, user: { id, picture, username }, token: token });
+    useEffect(() => {
+        const token = localStorage.getItem("token").split(".")[1];
+        if (!token) navigate("/signin");
+        let user = window.atob(token);
+        const { picture, id, username } = JSON.parse(user);
+
+        setData({ ...data, user: { id, picture, username }, token: token });
     }, []);
 
     useEffect(() => {
-      reloadData();       
+        reloadData();
+        verifyFollower();
     }, [publish, id])
 
     function warning() {
         alert("An error occured while trying to fetch the posts, please refresh the page");
+    }
+
+    function warningFollow() {
+        alert("An error occured during the process!");
     }
 
     function reloadData() {
@@ -45,14 +50,26 @@ export default function TimelineUser() {
         request = axios.get(`${API}/user/${id}`);
 
         request.then((response) => {
-          const { data } = response;
-          if (data[0]) setUser(data[0]);
-          if (data[1]) setPosts(data[1]);
-          console.log(data[0]);
-          console.log(data[0].username);
-          setLoad(true);
-         });
-         request.catch(warning);
+            const { data } = response;
+            if (data[0]) setUser(data[0]);
+            if (data[1]) setPosts(data[1]);
+            setLoad(true);
+        });
+        request.catch(warning);
+    }
+
+    function verifyFollower() {
+        let request = null;
+        setLoad(false);
+        request = axios.post(`${API}/follow/`, { following: id, follower: data.user.id });
+        if (Number(id) !== data.user.id) {
+            request.then((response) => {
+                const { data } = response;
+                if (data) setFollowing(data);
+                setLoad(true);
+            });
+            request.catch(warningFollow);
+        }
     }
 
     const tag = posts.length === 0 ? "There are no posts yet" : "";
@@ -61,66 +78,66 @@ export default function TimelineUser() {
 
     if (!load) {
         return (
-          <S.ContainerUser>
-            <S.Division>
-              <S.PostsColumnUser>
-                <S.LoaderUser>
-                  <LoadingPage />
-                </S.LoaderUser>
-              </S.PostsColumnUser>
-            </S.Division>
-            <Trending />
-          </S.ContainerUser>
+            <S.ContainerUser>
+                <S.Division>
+                    <S.PostsColumnUser>
+                        <S.LoaderUser>
+                            <LoadingPage />
+                        </S.LoaderUser>
+                    </S.PostsColumnUser>
+                </S.Division>
+                <Trending />
+            </S.ContainerUser>
         );
     } else {
         return (
-          <>
-            <Header picture={data.user.picture} />
-            <S.ContainerUser>
-              <S.H2User>
-                <S.UserInfo>
-                  <div>
-                    {img} <h2>{username}</h2>
-                  </div>
-                  <S.Follow>{following ? "Seguindo" : "Seguir"}</S.Follow>
-                </S.UserInfo>
-              </S.H2User>
-                <S.Division>
-                <S.PostsColumnUser>
-                  <h5>{tag}</h5>
-                  {posts.length !== 0 &&
-                    posts.map((post, index) => {
-                      return (
-                        <Posts
-                          postId={post.id}
-                          key={post.username + post.description + index}
-                          name={post.username}
-                          picture={post.picture}
-                          link={post.link}
-                          description={post.description}
-                          linkDescription={post.linkDescription}
-                          linkTitle={post.linkTitle}
-                          linkPicture={post.linkPicture}
-                          originalPost={post.originalPost}
-                          reposterName={post.reposterName}
-                          redirect={(val) => {
-                            val = val.replace("#", "");
-                            navigate(`/hashtag/${val}`);
-                          }}
-                          reloadPosts={reloadData}
+            <>
+                <Header picture={data.user.picture} />
+                <S.ContainerUser>
+                    <S.H2User>
+                        <S.UserInfo>
+                            <div>
+                                {img} <h2>{username}</h2>
+                            </div>
+                            <S.Follow following={following} isUser={Number(id) === data.user.id}>{following ? "Unfollow" : "Follow"}</S.Follow>
+                        </S.UserInfo>
+                    </S.H2User>
+                    <S.Division>
+                        <S.PostsColumnUser>
+                            <h5>{tag}</h5>
+                            {posts.length !== 0 &&
+                                posts.map((post, index) => {
+                                    return (
+                                        <Posts
+                                            postId={post.id}
+                                            key={post.username + post.description + index}
+                                            name={post.username}
+                                            picture={post.picture}
+                                            link={post.link}
+                                            description={post.description}
+                                            linkDescription={post.linkDescription}
+                                            linkTitle={post.linkTitle}
+                                            linkPicture={post.linkPicture}
+                                            originalPost={post.originalPost}
+                                            reposterName={post.reposterName}
+                                            redirect={(val) => {
+                                                val = val.replace("#", "");
+                                                navigate(`/hashtag/${val}`);
+                                            }}
+                                            reloadPosts={reloadData}
+                                        />
+                                    );
+                                })}
+                        </S.PostsColumnUser>
+                        <Trending
+                            redirect={(val) => {
+                                val = val.replace("#", "");
+                                navigate(`/hashtag/${val}`);
+                            }}
                         />
-                      );
-                    })}
-                </S.PostsColumnUser>
-                <Trending
-                  redirect={(val) => {
-                    val = val.replace("#", "");
-                    navigate(`/hashtag/${val}`);
-                  }}
-                />
-                </S.Division>
-            </S.ContainerUser>
-          </>
+                    </S.Division>
+                </S.ContainerUser>
+            </>
         );
     }
 }
